fa8ae2719bf25457871c3eabbab69f00d405193c
Juan Manuel Scilingo <juanscilingo@gmail.com>
2019-03-13 05:10:39 -0300

    Adds help for run command

--- src/commands/run.js
+++ src/commands/run.js
@@ -1,17 +1,33 @@
+import { RichEmbed } from "discord.js";
 import { VM } from "vm2";
 
-const CODE_REGEX = /```([\s\S]+)```/m;
+const CODE_REGEX = /```js([\s\S]+)```/m;
 
 const extractCode = message => {
   const matches = CODE_REGEX.exec(message.content);
   return matches ? matches[1] : null;
 };
 
+const sendHelp = message => {
+  const embed = new RichEmbed().setTitle("How to run code?").setColor(0xff0000)
+    .setDescription(`Please include a **javascript** code block in your message like this:
+      >run
+      **\\\`\\\`\\\`js**
+      const num1 = 2;
+      const num2 = 5;
+
+      num1 + num2
+      **\\\`\\\`\\\`**
+    `);
+
+  message.channel.send(embed);
+};
+
 const run = message => {
   const code = extractCode(message);
 
   if (!code) {
-    message.channel.send("Please include a code block");
+    sendHelp(message);
     return;
   }
   const vm = new VM({
